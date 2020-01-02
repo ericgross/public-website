c0385472f3a9cee4e1a27dff1548a52adeeca958
Eric Gross <github@ericgross.com>
2020-01-02 17:16:56 -0500

    Add spacing

--- spec/system/concepts_spec.rb
+++ spec/system/concepts_spec.rb
@@ -5,18 +5,22 @@ require 'rails_helper'
 describe 'Concepts' do
   it 'allows navigation of concepts' do
     visit root_path
+
     click_link 'Concepts'
     expect(page).to have_css('.concept', text: 'Quality')
     expect(page.all('.concept').count).to be > 50
+
     click_link 'Quality'
     expect(page).to have_css('.title', text: 'Quality')
     expect(page).to have_css('.content')
     expect(page).to have_css('.references')
+
     within('.references') do
       click_link('Domain driven design')
     end
     expect(page).to have_css('.title', text: 'Domain driven design')
     expect(page).to have_css('.referenced-by')
+
     within('.referenced-by') do
       click_link('Quality')
     end
