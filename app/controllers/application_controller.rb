1f0b4f32cec4a2a7aa791f0ca974cc5f09b02873
Eric Gross <github@ericgross.com>
2020-01-03 15:28:50 -0500

    Improve UI

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,22 @@
 class ApplicationController < ActionController::Base
   def related_concepts(to:)
-    return top_level_concepts unless current_concept(to: to)
+    return top_level_concepts unless to
 
     current_concept(to: to).references
   end
 
+  def related_from_concepts(to:)
+    return [] unless to
+
+    current_concept(to: to).referenced_by
+  end
+
   helper_method :related_concepts
+  helper_method :related_from_concepts
 
   private
 
-  def current_concept(to)
+  def current_concept(to:)
     Concept::WithId.new.result(id: to)
   end
 end
