a7e766466ebecb0b387e05a3092707f28c1fec7e
Eric Gross <github@ericgross.com>
2020-01-03 14:44:56 -0500

    Add home link

--- spec/system/concepts_spec.rb
+++ spec/system/concepts_spec.rb
@@ -6,19 +6,21 @@ describe 'Concepts' do
   it 'allows navigation of concepts' do
     visit root_path
 
+    within('.header') do
+      click_link 'Home'
+    end
+
     expect(page).to have_css('.concept', text: 'Quality')
     expect(page.all('.concept').count).to eq(5)
 
     click_link 'Quality'
     expect(page).to have_css('.title', text: 'Quality')
     expect(page).to have_css('.content')
-    expect(page).to have_css('.references')
 
     within('.references') do
       click_link('Domain driven design')
     end
     expect(page).to have_css('.title', text: 'Domain driven design')
-    expect(page).to have_css('.referenced-by')
 
     within('.referenced-by') do
       click_link('Quality')
